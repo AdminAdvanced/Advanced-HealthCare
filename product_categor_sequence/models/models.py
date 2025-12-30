dfa3dcbef8411ca7c88cd8e5b559f21de3cd788e
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2025-12-30 17:11:17 +0300

    Skip product code sequence logic in test mode

--- product_categor_sequence/models/models.py
+++ product_categor_sequence/models/models.py
@@ -13,7 +13,7 @@ class ProductTemplate(models.Model):
 
     @api.model_create_multi
     def create(self, vals_list):
-        if self.env.context.get('test_enable'):
+        if self.env.registry.in_test_mode():
             return super().create(vals_list)
 
         for vals in vals_list:
