c5feb8bf756755e51fec70a4e18887f63421b455
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-01-01 00:41:17 +0300

    commint view

--- product_categor_sequence/models/models.py
+++ product_categor_sequence/models/models.py
@@ -12,3 +12,24 @@ class ProductCategory(models.Model):
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        # لا نلمس أي create أثناء install / tests
+        if self.env.context.get('install_mode') or self.env.context.get('test_enable'):
+            return super().create(vals_list)
+
+        for vals in vals_list:
+            if vals.get('default_code'):
+                continue
+
+            categ_id = vals.get('categ_id')
+            if not categ_id:
+                continue
+
+            category = self.env['product.category'].browse(categ_id)
+            if not category or not category.x_product_sequence_id:
+                continue
+
+            vals['default_code'] = category.x_product_sequence_id.next_by_id()
+
+        return super().create(vals_list)
