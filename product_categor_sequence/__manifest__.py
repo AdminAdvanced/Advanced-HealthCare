abb3d210fff736f68a3dce94309b61c3299c2d83
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2025-12-31 23:24:28 +0300

    edit in product_categor_sequence 2

--- product_categor_sequence/__manifest__.py
+++ product_categor_sequence/__manifest__.py
@@ -11,8 +11,8 @@ Long description of module's purpose
     'version': '0.1',
     'depends': ['base', 'product'],
     'data': [
-        'views/views.xml',
         'views/product.xml',
+        'views/views.xml',
     ],
     "installable": True,
     "license": "LGPL-3",
