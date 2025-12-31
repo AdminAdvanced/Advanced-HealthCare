18b077566426a836a0708aa187a2d6eb7373e0b1
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2025-12-31 23:40:39 +0300

    edit in product_categor_sequence 4

--- product_categor_sequence/models/models.py
+++ product_categor_sequence/models/models.py
@@ -1,4 +1,4 @@
-from odoo import models, fields, api, exceptions
+from odoo import models, fields, api
 
 class ProductCategory(models.Model):
     _inherit = 'product.category'
@@ -8,16 +8,17 @@ class ProductCategory(models.Model):
         string='Product Sequence'
     )
 
+
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
     @api.model_create_multi
     def create(self, vals_list):
-        if self.env.context.get('test_enable'):
+        # لا نلمس أي create أثناء install / tests
+        if self.env.context.get('install_mode') or self.env.context.get('test_enable'):
             return super().create(vals_list)
 
         for vals in vals_list:
-            # لا نولّد كود إذا كان موجود
             if vals.get('default_code'):
                 continue
 
@@ -26,15 +27,9 @@ class ProductTemplate(models.Model):
                 continue
 
             category = self.env['product.category'].browse(categ_id)
-            if not category or not category.exists():
+            if not category or not category.x_product_sequence_id:
                 continue
 
-            sequence = category.x_product_sequence_id
-            if not sequence:
-                continue
-
-            vals['default_code'] = sequence.next_by_id()
+            vals['default_code'] = category.x_product_sequence_id.next_by_id()
 
         return super().create(vals_list)
-
-
