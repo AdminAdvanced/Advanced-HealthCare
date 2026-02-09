0fec1cb6c710af2a9f728dd0c8ab96daf3f1ef92
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-02-09 10:03:08 +0300

    Add default_packaging

---
+++ default_packaging/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "default_packaging",
+    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'description': """
+Long description of module's purpose
+    """,
+    'author': "My Company",
+    'website': "https://www.yourcompany.com",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base',"product","stock","sale"],
+    'data': [
+        'views/views.xml',
+    ],
+}
+
