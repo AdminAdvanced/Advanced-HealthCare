d64ad9bfe91a47cb0f90674cdfcc34c32b83410c
AdminAdvanced <Admin@advanced-me.com>
2026-02-19 13:57:41 +0300

    Update models.py2

--- search_translate/models/models.py
+++ search_translate/models/models.py
@@ -77,9 +77,11 @@ class ProductProduct(models.Model):
     def _name_search(self, name='', args=None, operator='ilike', limit=100, order=None):
         args = args or []
 
-        if name:
+                if name:
             domain = [
-                '|', '|', '|',
+                '|', '|', '|', '|', '|',
+                ('product_tmpl_id.name', operator, name),
+                ('product_tmpl_id.x_studio_product_name_ar', operator, name),
                 ('name', operator, name),
                 ('x_studio_product_name_ar', operator, name),
                 ('default_code', operator, name),
