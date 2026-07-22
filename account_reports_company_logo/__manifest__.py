e8d9ad44818bd779e003564ac89f851fde78a0d9
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-07-22 10:02:07 +0300

    add module account_reports_company_logo

---
+++ account_reports_company_logo/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Accounting Reports Company Logo",
+    "version": "18.0.1.0.0",
+    "category": "Accounting",
+    "summary": "Adds company logo to Accounting PDF reports",
+    "author": "Your Company",
+    "license": "LGPL-3",
+
+    "depends": [
+        "account_reports",
+    ],
+
+    "data": [
+        "views/account_reports_company_information.xml",
+    ],
+
+    "installable": True,
+    "application": False,
+}
