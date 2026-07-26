53dd73f6fc4641f4c37b3b7109265b09100f7829
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-07-26 21:10:51 +0300

    add lot invoice report.xml

--- invoice_report_custom/models/models.py
+++ invoice_report_custom/models/models.py
@@ -1,15 +1,22 @@
-# -*- coding: utf-8 -*-
 from odoo import models
 
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
+class AccountMoveLine(models.Model):
+    _inherit = "account.move.line"
 
-    def _get_invoiced_lot_values(self):
-        values = super()._get_invoiced_lot_values()
+    def get_invoice_lots(self):
+        self.ensure_one()
 
-        for val in values:
-            lot = self.env['stock.lot'].browse(val['lot_id'])
-            val['product_id'] = lot.product_id.id
+        lots = []
 
-        return values
+        for sale_line in self.sale_line_ids:
+            for move in sale_line.move_ids:
+                for move_line in move.move_line_ids:
+                    if move_line.lot_id:
+                        lots.append({
+                            "lot_name": move_line.lot_id.name,
+                            "quantity": move_line.quantity,
+                            "uom": move_line.product_uom_id.name,
+                        })
+
+        return lots
