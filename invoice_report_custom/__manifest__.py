b1ffaf9aaa7f9bf1ff53b19c66564477cbb76460
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-07-26 14:51:29 +0300

    edit تصغير حجم QR Code  invoice report.xml

--- invoice_report_custom/__manifest__.py
+++ invoice_report_custom/__manifest__.py
@@ -3,7 +3,8 @@
     'version': '18.0.1.0',
     'license': 'LGPL-3',
     'depends': ['account',
-                'l10n_gcc_invoice',],
+                'l10n_gcc_invoice',
+                'l10n_sa',],
     'data': [
         'views/invoice_report.xml',
     ],
