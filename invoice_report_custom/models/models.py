f45f070c8c5c665f8b8fe50f6c0575de267f104d
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-07-27 11:45:05 +0300

    edit invoice report custom

--- invoice_report_custom/models/models.py
+++ invoice_report_custom/models/models.py
@@ -4,26 +4,44 @@ from odoo import models
 class AccountMove(models.Model):
     _inherit = "account.move"
 
-    def get_invoice_line_lots(self, invoice_line):
-        """Return lots for a specific invoice line."""
+    def _get_invoiced_lot_values(self):
+        res = super()._get_invoiced_lot_values()
 
+        for lot_line in res:
+
+            lot = self.env["stock.lot"].browse(lot_line.get("lot_id"))
+
+            if lot.exists():
+
+                product = lot.product_id
+
+                lot_line.update({
+                    "product_name_en": product.name,
+
+                    "product_name_ar": (
+                        product.product_tmpl_id.x_studio_product_name_ar
+                        or product.name
+                    ),
+
+                    "uom_name_en": lot.product_uom_id.name,
+
+                    "uom_name_ar": (
+                        lot.product_uom_id.x_studio_unit_of_measure_ar
+                        or lot.product_uom_id.name
+                    ),
+                })
+
+        return res
+
+    def get_invoice_line_lots(self, invoice_line):
         self.ensure_one()
 
         result = []
 
         lot_values = self._get_invoiced_lot_values()
 
-        if not invoice_line.product_id:
-            return result
-
-        product_name = invoice_line.product_id.display_name
-
         for lot in lot_values:
-            if lot.get("product_name") == product_name:
-                result.append({
-                    "lot_name": lot.get("lot_name"),
-                    "quantity": lot.get("quantity"),
-                    "uom_name": lot.get("uom_name"),
-                })
+            if lot.get("product_name_en") == invoice_line.product_id.name:
+                result.append(lot)
 
         return result
