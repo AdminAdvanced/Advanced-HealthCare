3c0e5170f27130b10a46f8f560ec0577ae5a3aa8
Bushra Alamarnah <bushraalamarnah@Bushras-MacBook-Air.local>
2026-09-01 11:40:02 +0300

    edit module account_reports_company_logo/models

--- account_reports_company_logo/models/account_partner_ledger.py
+++ account_reports_company_logo/models/account_partner_ledger.py
@@ -6,7 +6,29 @@ from odoo import models
 class PartnerLedgerCustomHandler(models.AbstractModel):
     _inherit = "account.partner.ledger.report.handler"
 
-    def _get_report_line_partners(self, options, partner, partner_values, level_shift=0):
+    def _get_partner_display_name(self, partner):
+        """Return partner name according to the current user's language."""
+        if not partner:
+            return ""
+
+        partner_name = partner.name or ""
+
+        if (
+            self.env.user.lang
+            and self.env.user.lang.startswith("ar")
+            and partner.x_studio_name_ar
+        ):
+            partner_name = partner.x_studio_name_ar
+
+        return partner_name
+
+    def _get_report_line_partners(
+        self,
+        options,
+        partner,
+        partner_values,
+        level_shift=0,
+    ):
         line = super()._get_report_line_partners(
             options,
             partner,
@@ -15,7 +37,11 @@ class PartnerLedgerCustomHandler(models.AbstractModel):
         )
 
         if partner:
+            partner_name = self._get_partner_display_name(partner)
+
             if partner.contact_code:
-                line["name"] = f"{partner.contact_code} - {partner.name or ''}"
+                line["name"] = f"{partner.contact_code} - {partner_name}"
+            else:
+                line["name"] = partner_name
 
         return line
